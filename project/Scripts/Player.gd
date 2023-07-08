1b99345b007dc879b5a80601152c86c6844dc5d6
eaitken-scottlogic <eaitken@scottlogic.com>
2023-07-08 15:29:41 +0100

    Fix Lost Stuff

--- project/Scripts/Player.gd
+++ project/Scripts/Player.gd
@@ -9,31 +9,50 @@ var magnitudeMultiplier: float = 12
 # Mouse held position
 var mouseHeld: bool = false
 
+# The Indicator Arrow
 @onready var line := $Line2D
+
+var mainNode: Node2D
+
+var arrowScene: PackedScene = preload("res://Scenes/arrow.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	mainNode = get_tree().get_root().get_node("MainNode")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if mouseHeld:
-		line.show()
-		line.points[0] = to_local(self.position)
+		_showLine()
+		
+func _showLine():
+	line.show()
+	line.points[0] = to_local(self.position)
+	var magnitude: float = (get_global_mouse_position() - self.position).length()
+	if magnitude > maxForce:
+		var direction: Vector2 = (get_global_mouse_position() - self.position).normalized()
+		line.points[1] = to_local(self.position + (direction * maxForce))
+	else:
 		line.points[1] = to_local(get_global_mouse_position())
 
 func _addPlayerForce(force: Vector2):
 	self.linear_velocity = Vector2.ZERO
 	apply_impulse(force)
+	_spawnArrow()
+
+func _spawnArrow():
+	var newArrow = arrowScene.instantiate()
+	newArrow.position = self.position
+	mainNode.add_child(newArrow)
 
 func _input(event):
 	if event is InputEventMouseButton and !event.pressed:
 		mouseHeld = false
-		var launch: Vector2 = (self.position - get_global_mouse_position()).normalized()
-		var magnitude: float = (self.position - get_global_mouse_position()).length()
-		magnitude *= magnitudeMultiplier
+		var launch: Vector2 = (get_global_mouse_position() - self.position).normalized()
+		var magnitude: float = (get_global_mouse_position() - self.position).length()
 		if magnitude > maxForce:
 			magnitude = maxForce
+		magnitude *= magnitudeMultiplier
 		_addPlayerForce(launch * magnitude)
 		line.hide()
 	elif event is InputEventMouseButton and event.pressed:
