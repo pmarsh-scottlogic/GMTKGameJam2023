76b6f88f2aa7503c9bd51ea3d37398060a80712c
eaitken-scottlogic <eaitken@scottlogic.com>
2023-07-08 14:00:08 +0100

    Add basic indicator line

--- project/Scripts/Player.gd
+++ project/Scripts/Player.gd
@@ -6,22 +6,35 @@ var maxForce: float = 1500
 # Multiplier for magnitude
 var magnitudeMultiplier: float = 2
 
+# Mouse held position
+var mouseHeld: bool = false
+
+@onready var line := $Line2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if mouseHeld:
+		line.show()
+		line.points[0] = to_local(self.position)
+		line.points[1] = to_local(get_global_mouse_position())
 
 func _addPlayerForce(force: Vector2):
 	apply_impulse(force)
 
 func _input(event):
 	if event is InputEventMouseButton and !event.pressed:
+		mouseHeld = false
 		var launch: Vector2 = (self.position - get_global_mouse_position()).normalized()
 		var magnitude: float = (self.position - get_global_mouse_position()).length()
 		magnitude *= magnitudeMultiplier
 		if magnitude > maxForce:
 			magnitude = maxForce
 		_addPlayerForce(launch * magnitude)
+		line.hide()
+	elif event is InputEventMouseButton and event.pressed:
+		mouseHeld = true
+			
