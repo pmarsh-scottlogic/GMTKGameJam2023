ffb36f48f3d40ced9660e5ba95514fb8b0fa9901
Peter Marsh <118171430+pmarsh-scottlogic@users.noreply.github.com>
2023-07-09 16:40:21 +0100

    added intro stuff (#50)

--- project/Scripts/HandleCutScene.gd
+++ project/Scripts/HandleCutScene.gd
@@ -1,16 +1,14 @@
-extends TileMap
+extends Node
 
-@onready var camera = get_node("../Camera2D")
-@export var driftSpeed : float = 0
-var age = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$SpinPlayer.play("SpinPlayer")
+	$fallPlayer.play("fallPlayer")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	age += delta
-	position.x = camera.position.x * 0.9 + age * delta * 100
 	pass
+ 
