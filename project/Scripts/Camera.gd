b0a7edf6dea811ce151d75ba8abac775619c8e12
Peter Marsh <pmarsh@scottlogic.com>
2023-07-09 16:37:32 +0100

    added intro stuff

--- project/Scripts/Camera.gd
+++ project/Scripts/Camera.gd
@@ -4,12 +4,16 @@ extends Camera2D
 @export var maxSpeed : float = 10
 @export var catchupSpeed : float = 5
 @export var lookAheadDistance := 250
+@export var timeUntilFollow : float = 0
+var age = 0
+var suppressFollowPlayer = false;
 
 @onready var playerNode = get_tree().get_root().get_node("Player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if timeUntilFollow > 0 and followPlayer:
+		suppressFollowPlayer = true
 	
 func getFollowPlayerTarget():
 	var playerPos : Vector2 = get_node("../Player").position
@@ -21,13 +25,22 @@ func getFollowPlayerTarget():
 	return target
 
 func getTarget():
-	if followPlayer:
+	if followPlayer and not suppressFollowPlayer:
 		return getFollowPlayerTarget()
 	else:
 		return position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	age += delta
+	if timeUntilFollow > 0 and followPlayer:
+		if age < timeUntilFollow:
+			print("suppress")
+			suppressFollowPlayer = true
+		else:
+			print("don't suppress")
+			suppressFollowPlayer = false
+	
 	var target : Vector2 = getTarget()
 	var positionToTarget : Vector2 = target - position
 	var movementAmount = min(positionToTarget.length() * catchupSpeed * 0.001 * delta, maxSpeed * delta)
