a4b77bd0bdfd079d6bb8ffc83f0a1fe55d133651
Peter Marsh <pmarsh@scottlogic.com>
2023-07-09 17:27:58 +0100

    added new sounds

---
+++ project/Scripts/introLevelAutioManager.gd
@@ -0,0 +1,17 @@
+extends AudioStreamPlayer2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT :
+		if !event.pressed:
+			$CrowdPlayer.stop()
+			$DrumrollPlayer.stop()
